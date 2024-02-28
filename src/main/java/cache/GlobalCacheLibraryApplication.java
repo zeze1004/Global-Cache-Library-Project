1e7093656892b292ae5be311a26281883ef831e9
zeze <thwjd2717@gmail.com>
2024-02-28 14:29:05 +0900

    chore: 초기 셋팅

---
+++ src/main/java/cache/GlobalCacheLibraryApplication.java
@@ -0,0 +1,13 @@
+package cache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobalCacheLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlobalCacheLibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cache/GlobalCacheLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package cache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlobalCacheLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
